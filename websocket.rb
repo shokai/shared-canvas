8110dd3838bf22704480a445460c78b58f31e521
Sho Hashimoto <hashimoto@shokai.org>
2011-07-22 17:45:57 +0900

    store logs as JSON

--- websocket.rb
+++ websocket.rb
@@ -20,13 +20,8 @@ EM::run do
 
   EM::WebSocket.start(:host => "0.0.0.0", :port => port) do |ws|
     ws.onopen{
-      sid = @channel.subscribe{|mes|
-        begin
-          data = JSON.parse(mes)
-          ws.send(data.to_json) if @sids[sid] == data['img_url']
-        rescue => e
-          STDERR.puts e
-        end
+      sid = @channel.subscribe{|data|
+        ws.send(data.to_json) if data['img_url'] == @sids[sid]
       }
       puts "<#{sid}> connected!!"
       ws.send({:type => :init, :sid => sid}.to_json)
@@ -37,17 +32,12 @@ EM::run do
           data = JSON.parse(mes)
           if data['type'].to_s == 'init'
             @sids[sid] = data['img_url']
-            @logs.each{|mes|
-              begin
-                tmp = JSON.parse(mes)
-                ws.send(tmp.to_json) if tmp['img_url'] == @sids[sid]
-              rescue => e
-                STDERR.puts e
-              end
+            @logs.each{|data|
+              ws.send(data.to_json) if data['img_url'] == @sids[sid]
             }
           else
             data[:sid] = sid
-            @channel.push(data.to_json)
+            @channel.push(data)
           end
         rescue => e
           STDERR.puts e
